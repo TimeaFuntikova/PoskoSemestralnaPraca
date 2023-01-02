3a46ee510bc8d7a3a4ead074958090e70b5cc95e
timea <timkaselli@gmail.com>
2023-01-02 23:06:15 +0100

    client - server architecture - skratch

---
+++ k_s_definitions.h
@@ -0,0 +1,22 @@
+//
+// Created by timka on 2. 1. 2023.
+//
+
+#ifndef K_DEFINITIONS_H
+#define	K_DEFINITIONS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define BUFFER_LENGTH 300
+extern char *endMsg;
+
+void printError(char *str);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* K_DEFINITIONS_H */
+
